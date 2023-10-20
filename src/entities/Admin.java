fa333f4bb4f5f0ae555623cb855d58c9ea9031d3
Faithful Aborisade <130511882+faithfulaborisade1@users.noreply.github.com>
2023-10-20 10:12:48 +0100

    Add files via upload

---
+++ src/entities/Admin.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Admin {
+
+    public void createStaff(String name, String address, String phoneNumber, String publication) {
+		
+	}
+	
+	public String readStaff() {
+		return "";
+	}
+	
+	public void updateStaff(String name, String address, String phoneNumber, String publication) {
+		
+	}
+	
+	public void deleteStaff() {
+		
+	}
+    
+}
